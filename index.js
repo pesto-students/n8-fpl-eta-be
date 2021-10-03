d4759b0f3b5ca39b8fbf703a20fc6e5e6ae84746
Neil Doshi <neil@nzymtechnologies.com>
2021-10-03 07:05:48 +0530

    bug fix for index.js, comment unreachable files

--- index.js
+++ index.js
@@ -3,18 +3,14 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const config = require('./config');
-const studentRoutes = require('./routes/student-routes');
-const pingRoutes = require('./routes/ping-routes');
+// const studentRoutes = require('./routes/student-routes');
+// const pingRoutes = require('./routes/ping-routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
-
-app.use('/', landingPage);
-app.use('/api', studentRoutes.routes);
-app.use('/ping', pingRoutes.routes);
 
 const landingPage = async (req, res, next) => {
     try {
@@ -23,6 +19,11 @@ const landingPage = async (req, res, next) => {
         res.status(400).send(error.message);
     }
 }
+
+app.use('/', landingPage);
+// app.use('/api', studentRoutes.routes);
+// app.use('/ping', pingRoutes.routes);
+
 
 
 app.listen(config.port, () => console.log('App is listening on ' + config.port));
